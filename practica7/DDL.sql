fe3883301cefe4352dcf75516f5ed7682fdf648d
juan759 <jasso.antonio2001@gmail.com>
2021-12-07 17:18:42 -0600

    Agregada la política Cascade para manejar foreign keys.

--- practica7/DDL.sql
+++ practica7/DDL.sql
@@ -120,33 +120,45 @@ LLAVES PRIMARIAS, FORANEAS Y COMPUESTAS
 ALTER TABLE cliente ADD CONSTRAINT cliente_pk PRIMARY KEY (curp_cliente);
 
 ALTER TABLE tarjeta ADD CONSTRAINT tarjeta_pk PRIMARY KEY (numero_tarjeta);
-ALTER TABLE tarjeta ADD CONSTRAINT cliente_fk FOREIGN KEY (curp_cliente) REFERENCES cliente(curp_cliente);
+ALTER TABLE tarjeta ADD CONSTRAINT cliente_fk FOREIGN KEY (curp_cliente)
+REFERENCES cliente(curp_cliente) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE telefono ADD CONSTRAINT telefono_pk PRIMARY KEY (rfc, telefono);
-ALTER TABLE telefono ADD CONSTRAINT proveedor_fk FOREIGN KEY (rfc) REFERENCES proveedor(rfc);
+ALTER TABLE telefono ADD CONSTRAINT proveedor_fk FOREIGN KEY (rfc)
+REFERENCES proveedor(rfc) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE proveedor ADD CONSTRAINT proveedor_pk PRIMARY KEY (rfc);
 
 ALTER TABLE categoria ADD CONSTRAINT categoria_pk PRIMARY KEY (id_categoria);
 
 ALTER TABLE producto ADD CONSTRAINT producto_pk PRIMARY KEY (codigo_Barras);
-ALTER TABLE producto ADD CONSTRAINT proveedor_fk FOREIGN KEY (rfc) REFERENCES proveedor(rfc);
+ALTER TABLE producto ADD CONSTRAINT proveedor_fk FOREIGN KEY (rfc)
+REFERENCES proveedor(rfc) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE Repartidor ADD CONSTRAINT repartidor_pk PRIMARY KEY (curp_repartidor);
-ALTER TABLE Repartidor ADD CONSTRAINT supervisor_fk FOREIGN KEY (curp_supervisor) REFERENCES Repartidor(curp_supervisor);
+ALTER TABLE Repartidor ADD CONSTRAINT supervisor_fk FOREIGN KEY (curp_supervisor)
+REFERENCES Repartidor(curp_repartidor) ON DELETE ON UPDATE CASCADE;
 
 ALTER TABLE envio_normal ADD CONSTRAINT envio_normal_pk PRIMARY KEY (id_envio_normal);
-ALTER TABLE envio_normal ADD CONSTRAINT repartidor_fk FOREIGN KEY (curp_repartidor) REFERENCES Repartidor(curp_repartidor);
+ALTER TABLE envio_normal ADD CONSTRAINT repartidor_fk FOREIGN KEY (curp_repartidor)
+REFERENCES Repartidor(curp_repartidor) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE envio_express ADD CONSTRAINT envio_express_pk PRIMARY KEY (id_envio_express);
-ALTER TABLE envio_express ADD CONSTRAINT repartidor_fk FOREIGN KEY (curp_repartidor) REFERENCES Repartidor(curp_repartidor);
+ALTER TABLE envio_express ADD CONSTRAINT repartidor_fk FOREIGN KEY (curp_repartidor)
+REFERENCES Repartidor(curp_repartidor) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE Pedido ADD CONSTRAINT pedido_pk PRIMARY KEY (ID_Pedido);
-ALTER TABLE Pedido ADD CONSTRAINT cliente_fk FOREIGN KEY (curp_cliente) REFERENCES cliente(curp_cliente);
-ALTER TABLE Pedido ADD CONSTRAINT envio_normal_fk FOREIGN KEY (id_envio_normal) REFERENCES envio_normal(id_envio_normal);
-ALTER TABLE Pedido ADD CONSTRAINT envio_express_fk FOREIGN KEY (id_envio_express) REFERENCES envio_express(id_envio_express);
+ALTER TABLE Pedido ADD CONSTRAINT cliente_fk FOREIGN KEY (curp_cliente)
+REFERENCES cliente(curp_cliente) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Pedido ADD CONSTRAINT envio_normal_fk FOREIGN KEY (id_envio_normal)
+REFERENCES envio_normal(id_envio_normal) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Pedido ADD CONSTRAINT envio_express_fk FOREIGN KEY (id_envio_express)
+REFERENCES envio_express(id_envio_express) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE Tener ADD CONSTRAINT pedido_fk FOREIGN KEY (ID_Pedido) REFERENCES Pedido(ID_Pedido);
-ALTER TABLE Tener ADD CONSTRAINT producto_fk FOREIGN KEY (codigo_Barras) REFERENCES producto(codigo_Barras);
+ALTER TABLE Tener ADD CONSTRAINT pedido_fk FOREIGN KEY (ID_Pedido)
+REFERENCES Pedido(ID_Pedido) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Tener ADD CONSTRAINT producto_fk FOREIGN KEY (codigo_Barras)
+REFERENCES producto(codigo_Barras) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE producto ADD CONSTRAINT categoria_fk FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria);
+ALTER TABLE producto ADD CONSTRAINT categoria_fk FOREIGN KEY (id_categoria)
+REFERENCES categoria(id_categoria) ON DELETE CASCADE ON UPDATE CASCADE;
